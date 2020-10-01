0eeb66ab91338d57b1187855409d6c9f2bfb114c
Rohit0301 <60229265+Rohit0301@users.noreply.github.com>
2020-10-01 19:35:53 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+# main.py
+# at the end point / call method hello_world which returns "Hello World!"
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+#Main function
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
