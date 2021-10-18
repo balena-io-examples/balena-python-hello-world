0a31796ab4abf1824ae48d0debb7cf205da5b28e
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2021-10-19 02:00:51 +0530

    Use updated balena example projects
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
